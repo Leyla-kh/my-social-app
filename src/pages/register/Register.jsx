e063ad23a946d2fcd67178348773d25bdd9b0e8a
Leyla-kh <98767820+Leyla-kh@users.noreply.github.com>
2023-06-09 00:55:54 +0330

    updated-2

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -20,7 +20,7 @@ export default function Register() {
         password: password.current.value,
       };
       try {
-        await axiosInstance.post("/auth/register", JSON.stringify(user));
+        await axiosInstance.post("/auth/register", user);
         history("/login");
       } catch (error) {
         console.log(error);
